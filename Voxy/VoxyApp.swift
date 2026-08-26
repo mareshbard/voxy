8410e8be4683daff02f9a8adc48f30c4b6ad289f
mareshbard <leticiapereirag59@gmail.com>
2026-08-26 10:34:15 -0300

    Initial Commit

---
+++ Voxy/VoxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxyApp.swift
+//  Voxy
+//
+//  Created by Leticia Gomes on 26/08/26.
+//
+
+import SwiftUI
+
+@main
+struct VoxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
